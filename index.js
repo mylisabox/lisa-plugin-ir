a7031c8b757818519a721c21bd1b906416e7aad1
Jimmy Aumard <Jimmy.Aumard@nestle.com>
2017-09-10 13:20:07 +0200

    add DEVICE_TURN_ON support

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
 const Plugin = require('lisa-plugin')
 
 module.exports = class IRPlugin extends Plugin {
+
   interact(action, infos) {
+    const device = infos.fields.device
+    if (device && device.pluginName !== this.fullName) {
+      return Promise.resolve()
+    }
     switch (action) {
+      case "DEVICE_TURN_ON":
+        return this.drivers['ir'].setDeviceValue(device)
       case "SEND_IR_COMMAND":
         return this.drivers['ir'].setDeviceValue(null, infos.fields.remote, infos.fields.action)
       default:
