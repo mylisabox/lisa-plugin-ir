41e102af21a1b59b6036ac0f07291ee2bc4d9e4a
jaumard <jimmy.aumard@gmail.com>
2019-10-05 15:19:16 +0200

    continue migration to flutter widget protocol

--- config/devices.js
+++ config/devices.js
@@ -15,29 +15,33 @@ module.exports = [
     type: 'other',
     template: template,
     image: 'ir.jpg',
-    settings: [
-      {
-        controlType: 'textbox',
-        type: 'text',
-        name: 'remote',
-        label: {
-          en: 'Remote name',
-          fr: 'Nom de la télécommande'
+    settings: {
+      'type': 'column',
+      'crossAxisAlignment': 3,
+      'children': [
+        {
+          controlType: 'textbox',
+          type: 'text',
+          name: 'remote',
+          label: {
+            en: 'Remote name',
+            fr: 'Nom de la télécommande'
+          },
+          required: true,
+          private: true
         },
-        required: true,
-        private: true
-      },
-      {
-        controlType: 'textbox',
-        type: 'text',
-        name: 'action',
-        label: {
-          en: 'Remote command',
-          fr: 'Nom de la touche'
-        },
-        required: true,
-        private: true
-      }
-    ]
+        {
+          controlType: 'textbox',
+          type: 'text',
+          name: 'action',
+          label: {
+            en: 'Remote command',
+            fr: 'Nom de la touche'
+          },
+          required: true,
+          private: true
+        }
+      ],
+    }
   }
 ]
