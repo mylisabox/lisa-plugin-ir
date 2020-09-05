78ede8b2a4732f88a836e2989e2ee7b72bd592b2
jaumard <jimmy.aumard@gmail.com>
2020-09-05 19:02:43 +0200

    fix case sensitive commands

--- drivers/ir.js
+++ drivers/ir.js
@@ -7,14 +7,14 @@ module.exports = class IRDriver extends Driver {
   setDeviceValue(device, key, value) {
     if (device) {
       let command = device.data.action
-      if (command.indexOf('key') === -1) {
+      if (command.toLowerCase().indexOf('key') === -1) {
         command = 'KEY_' + command.trim().toUpperCase()
       }
       return this.lisa.ir.send(device.data.remote, command)
     }
     else {
       let command = value
-      if (command && command.indexOf('key') === -1) {
+      if (command && command.toLowerCase().indexOf('key') === -1) {
         command = 'KEY_' + command.trim().toUpperCase()
       }
       return this.lisa.ir.send(key, command)
